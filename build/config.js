690085447e190c1847b74cd748c2325f557e6e2b
Soyeon Park <iovethdus@gmail.com>
2026-08-12 11:43:11 +0900

    Delete build directory

--- build/config.js
+++
@@ -1,5 +0,0 @@
-// GitHub Pages 배포 후에도 이 파일의 URL만 수정하면 Google Sheets 연동이 가능합니다.
-// 예: googleScriptUrl: 'https://script.google.com/macros/s/DEPLOYMENT_ID/exec'
-window.__PROJECT3_CONFIG__ = {
-  googleScriptUrl: ''
-};
