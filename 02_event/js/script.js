04c42ff543431508cbbfa0b65194a926b855aaf2
Soyeon Park <iovethdus@gmail.com>
2026-08-19 14:05:54 +0900

    Add files via upload

---
+++ 02_event/js/script.js
@@ -0,0 +1,31 @@
+$(function name(params) {
+    $(".btn1").click(function(){
+    $(".parent .box1").hide()
+    })
+})
+
+$(function name(params) {
+    $(".btn2").click(function(){
+    $(".parent .box1").show()
+    })
+})
+
+$(function name(params) {
+    $(".btn3").click(function(){
+    $(".parent .box2").toggle()
+    })
+})
+
+$(function name(params) {
+    $(".btn4").click(function(){
+    $(".parent .box3").width(400)
+    $(".parent .box3").height(400)
+    })
+})
+
+$(function name(params) {
+    $(".btn5").click(function(){
+    $(".parent .box3").width(200)
+    $(".parent .box3").height(200)
+    })
+})
