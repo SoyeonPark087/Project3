190d6298555e8a44db9e0b8a4caaa641f6aaf647
Soyeon Park <iovethdus@gmail.com>
2026-08-20 17:33:18 +0900

    Delete 09_tab directory

--- 09_tab/js/script.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $(".tab li").click(function(){
-        let num = $(this).index()
-        console. log(num)
-        $(".tab li").removeClass("on")
-        $(this).addClass("on")
-    })
-})
