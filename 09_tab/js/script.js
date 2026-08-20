03c4f18fcf7c4b33dcbfc008afd0515f23a5c6a8
Soyeon Park <iovethdus@gmail.com>
2026-08-20 17:31:20 +0900

    Add files via upload

---
+++ 09_tab/js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(".tab li").click(function(){
+        let num = $(this).index()
+        console. log(num)
+        $(".tab li").removeClass("on")
+        $(this).addClass("on")
+    })
+})
